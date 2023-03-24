58648a908978c3426f9352942e3fdb4942ad596e
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-03-24 22:26:32 +0100

    making progress

--- my-app/src/home/home.jsx
+++ my-app/src/home/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Black from "../assets/black.mp4";
 import Sunset from "../assets/sunset.mp4";
+import Handshake from "../assets/shakinghands.png";
 
 const Home = () => {
   return (
@@ -11,10 +12,15 @@ const Home = () => {
         loop
         src={Sunset}
         className="opacity-5 bg-transparent"></video>
-      <div className="text-white z-10 absolute top-32 left-52 text-2xl">
-        <p className="text-base text-cyan-300 py-16`">hi my name is</p>
-        <h1>PAUL RICHARD,</h1>
-        <h1>WEB DEVELOPER</h1>
+      <div className="text-white z-10 absolute top-32 left-52 ">
+        <div>
+          <p className="text-base text-cyan-300 mb-6 ">hi my name is</p>
+          <h1 className="text-7xl mb-6 ">PAUL RICHARD,</h1>
+          <h1 className="text-7xl mb-6 ">WEB DEVELOPER</h1>
+        </div>
+        <span>
+          <img src={Handshake} alt="#" className="" />
+        </span>
       </div>
     </div>
   );
