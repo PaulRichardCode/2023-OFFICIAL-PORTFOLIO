f9b942cf96b8e2dd32e906b92c41cc177feaf762
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-03-24 21:47:05 +0100

    jj

--- my-app/src/home/home.jsx
+++ my-app/src/home/home.jsx
@@ -4,8 +4,8 @@ import Sunset from "../assets/sunset.mp4";
 
 const Home = () => {
   return (
-    <div className="relative w-full">
-      <video autoPlay muted loop src={Black} className="opacity-20"></video>
+    <div className="relative w-full mb-24">
+      {/* <video autoPlay muted loop src={Sunset} className="opacity-20 "></video> */}
       <div className="text-white z-10 absolute left-1/2 top-1/2 text-2xl">
         <h1>This is the Home</h1>
         <p>Lets go bro</p>
