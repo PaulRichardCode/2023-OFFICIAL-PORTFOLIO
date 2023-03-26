7b8b243b455469e224c93f8841a3a5219674295d
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-03-26 21:10:57 +0100

    j

--- my-app/src/home/home.jsx
+++ my-app/src/home/home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
         className="opacity-5 py-16"></video>
 
       {/* Home */}
-      <div className="text-white my-24 z-10 absolute top-32 left-52 md:flex md:items-center md:justify-center text-left">
+      <div className="text-white my-24 z-10 absolute top-24 left-52 md:flex md:items-center md:justify-center text-left">
         <div className="">
           <p className="text-lg font-medium pl-1 text-cyan-300/90 mb-6 ">
             Hi, my name is
@@ -25,19 +25,20 @@ const Home = () => {
           <h1 className="text-6xl mb-6 text-gray-400 font-bold">
             I build things for the web.
           </h1>
-          <p className="text-lg pl-1 text-gray-400 font-medium mb-6 w-2/3">
+          <p className="text-lg pl-1 text-gray-400 font-medium mb-6 w-4/5">
             I’m a software engineer specializing in building (and occasionally
             designing) exceptional digital experiences. Currently, I’m focused
             on building accessible services for people around the world.
           </p>
-          <button
+          <a
+            href="#"
             type="button"
-            class="text-cyan-300 border border-cyan-300 hover:bg-red-50/10 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm px-5 py-2.5 text-center mr-3 md:mr-0 ">
-            Resume
-          </button>
+            class="text-cyan-300 border border-cyan-300 hover:bg-red-50/10 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm px-16 py-5  text-center mr-3 md:mr-0 ">
+            Contact Me!
+          </a>
         </div>
         {/* handshake image */}
-        <div className="w-full">
+        <div className="w-full basis-4/5">
           <img src={Handshake} alt="#" className="opacity-75 w-96" />
         </div>
       </div>
