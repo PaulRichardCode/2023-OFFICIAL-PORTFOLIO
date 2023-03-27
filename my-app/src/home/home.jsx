3250649021efcbd1f6f37d7846c7948c3812cd92
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-03-27 20:51:30 +0100

    making stuff responsive

--- my-app/src/home/home.jsx
+++ my-app/src/home/home.jsx
@@ -6,20 +6,21 @@ import Handshake from "../assets/shakinghands.png";
 const Home = () => {
   return (
     <div className="relative w-full bg-gray-900/50 ">
-      <video
-        autoPlay
-        muted
-        loop
-        src={Black}
-        className="opacity-5 py-16"></video>
-
+      <div>
+        <video
+          autoPlay
+          muted
+          loop
+          src={Black}
+          className="opacity-5 py-16 w-full "></video>
+      </div>
       {/* Home */}
-      <div className="text-white my-24 z-10 absolute top-24 left-52 md:flex md:items-center md:justify-center text-left">
+      <div className="text-white md:my-24 z-10 absolute top-24 md:left-52 left-5 md:flex md:items-center md:justify-center text-left">
         <div className="">
           <p className="text-lg font-medium pl-1 text-cyan-300/80 mb-5 ">
             Hi, my name is
           </p>
-          <h1 className="text-7xl mb-6 text-gray-300 font-bold">
+          <h1 className="md:text-7xl text-5xl mb-6 text-gray-300 font-bold">
             Paul Richard.
           </h1>
           <h1 className="text-6xl mb-6 text-gray-400 font-bold">
