08435b87583e4df15d6f217a727abcf4b7cfc9b0
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-04-01 08:36:05 +0100

    w

--- my-app/src/link/link.jsx
+++ my-app/src/link/link.jsx
@@ -1,26 +1,43 @@
 import React from "react";
 import { FiGithub } from "react-icons/fi";
+import { FiTwitter } from "react-icons/fi";
+import { FiLinkedin } from "react-icons/fi";
+import { FiInstagram } from "react-icons/fi";
+import { FiMail } from "react-icons/fi";
 
 const Link = () => {
   return (
-    <div className="max-w-2xl">
-      <ul className="relative top-96 left-16">
+    <div className="max-w-2xl fixed">
+      <ul className="absolute z-50 top-96 left-16">
         <li>
-          <a href="#">
-            <FiGithub className="text-gray-300 text-2xl md:block hidden cursor-pointer" />
+          <a href="https://github.com/PaulRichardCode" target={"_blank"}>
+            <FiGithub className="text-gray-300 text-2xl md:block hidden cursor-pointer mb-7" />
           </a>
         </li>
         <li>
-          <a href="#"></a>
+          <a href="https://twitter.com/RichC0DE" target={"_blank"}>
+            <FiTwitter className="text-gray-300 text-2xl md:block hidden cursor-pointer mb-7" />
+          </a>
         </li>
         <li>
-          <a href="#"></a>
+          <a
+            href="https://www.linkedin.com/in/richard-paul-11321124b/"
+            target={"_blank"}>
+            <FiLinkedin className="text-gray-300 text-2xl md:block hidden cursor-pointer mb-7" />
+          </a>
         </li>
         <li>
-          <a href="#"></a>
+          <a href="https://www.instagram.com/richardpaul06/" target={"_blank"}>
+            <FiInstagram className="text-gray-300 text-2xl md:block hidden cursor-pointer mb-7" />
+          </a>
         </li>
         <li>
-          <a href="#"></a>
+          <a href="https://paulrichardevs@gmail.com" target={"_blank"}>
+            <FiMail className="text-gray-300 text-2xl md:block hidden cursor-pointer mb-7" />
+          </a>
+        </li>
+        <li className="flex items-center justify-center">
+          <div className="w-1 bg-cyan-200 h-screen overflow-hidden"></div>
         </li>
       </ul>
     </div>
