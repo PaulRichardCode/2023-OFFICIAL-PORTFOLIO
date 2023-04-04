c874e6fb451d76a210d8717d94e5bf08cc388e23
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-04-04 09:05:36 +0100

    ss

--- my-app/src/about/about.jsx
+++ my-app/src/about/about.jsx
@@ -5,7 +5,7 @@ import { FiUser } from "react-icons/fi";
 
 const About = () => {
   return (
-    <div className="my-20 w-2/3 mx-auto ">
+    <div className="my-20 w-3/4 mx-auto ">
       <div className="pl-44">
         <h1 className="text-gray-300 text-3xl mb-10 mr-8 w-11/12 font-semibold tracking-tight flex items-center justify-end">
           <span className="text-cyan-300 text-2xl pr-4">01.</span> About me{" "}
@@ -13,7 +13,9 @@ const About = () => {
         </h1>
       </div>
 
-      <div className="flex justify-around">
+      {/* image */}
+
+      <div className="flex justify-around items-start">
         <div className="opacity-90">
           <img
             src={Handy}
@@ -22,32 +24,58 @@ const About = () => {
           />
         </div>
         {/* Experience client and projects completed */}
-        <div className="lg:w-1/2 md:w-2/3 flex flex-col py-6 justify-around text-gray-300">
-          <div className="flex md:flex-row flex-col justify-between w-full">
+        <div className="lg:w-1/2 md:w-2/3 flex flex-col py-6 justify-around  text-gray-300">
+          <div className="flex md:flex-row flex-col justify-between w-full mb-10">
             <div className="flex flex-col justify-around py-8 items-center rounded-2xl bg-gray-700/10 w-40 h-40">
               <FiAward className="text-3xl mb-4 text-cyan-300" />
-              <h2 className="text-lg">Experience</h2>
-              <p className="text-xs text-gray-300/50">3+ years of working</p>
+              <h2 className="text-base font-medium">Experience</h2>
+              <p className="text-xs text-gray-300/50">2+ years of working</p>
             </div>
             <div className="flex flex-col justify-around py-8 items-center rounded-2xl bg-gray-700/10 w-40 h-40">
               <FiUser className="text-3xl mb-4 text-cyan-300" />
-              <h2 className="text-lg">Clients</h2>
+              <h2 className="text-base font-medium">Clients</h2>
               <p className="text-xs text-gray-300/50">50+ Worldwide</p>
             </div>
             <div className=" flex flex-col justify-around py-8 items-center rounded-2xl bg-gray-700/10 w-40 h-40">
               <FiFolder className="text-3xl mb-4 text-cyan-300" />
-              <h2 className="text-lg">Projects</h2>
+              <h2 className="text-base font-medium">Projects</h2>
               <p className="text-xs text-gray-300/50">25+ Completed</p>
             </div>
           </div>
           {/* about me text */}
-          <p>
-            Hello! My name is Richard and I enjoy creating things that live on
-            the internet. My interest in web development started back in 2019
-            when I decided to try editing custom Tumblr themes — turns out
-            hacking together a custom reblog button taught me a lot about HTML &
-            CSS!{" "}
-          </p>
+          <div className="text-gray-400/80">
+            <p className="mb-6">
+              Hello! My name is Richard and I would describe myself as a highly
+              skilled professional with a passion for creating beautiful and
+              functional user interfaces. I have a deep understanding of
+              front-end technologies such as HTML, CSS, and JavaScript.
+            </p>
+            <p className="mb-6">
+              {" "}
+              I am always up to date with the latest trends and practices in web
+              development.
+            </p>
+            <p>
+              I’ve had the privilege of working at an
+              <a
+                href="https://us.mullenlowe.com/"
+                className="text-cyan-300 hover:underline hover:transition-all">
+                {" "}
+                advertising agency
+              </a>
+              , a{" "}
+              <a
+                href="https://starry.com/"
+                className="text-cyan-300 hover:underline hover:transition-all">
+                start-up
+              </a>
+              <a
+                href="https://dangote.com/"
+                className="text-cyan-300 hover:underline hover:transition-all">
+                , a huge corporation.
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     </div>
