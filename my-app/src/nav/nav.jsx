daa14bf4a2cf713fe7d664356b07cd4a4cda9d03
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-04-07 14:10:57 +0100

    ok

--- my-app/src/nav/nav.jsx
+++ my-app/src/nav/nav.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import About from "../about/about";
 import "./nav.css";
 import Home from "../home/home";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
 const Nav = () => {
   /* Functionality */
@@ -69,7 +70,8 @@ const Nav = () => {
               class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 ">
               <li>
                 <a
-                  href={About}
+                  to="/about"
+                  href
                   class="cursor-pointer block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-cyan-300 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 "
                   aria-current="page">
                   <span className="text-cyan-300">01.</span> About
