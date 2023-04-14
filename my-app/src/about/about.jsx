8ca581f5c1a76f50cf89b94815bb64189a345485
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-04-14 20:09:58 +0100

    d

--- my-app/src/about/about.jsx
+++ my-app/src/about/about.jsx
@@ -17,17 +17,17 @@ const About = () => {
       {/* image */}
 
       <div className="md:flex md:justify-around md:items-start">
-        <div className="opacity-90">
+        <div className="md:opacity-90 opacity-20 md:left-0 left-1/2 mt-32 top-4/5 md:rotate-45 -rotate-180 md:relative absolute">
           <img
             src={Handy}
             alt="pointing hand"
-            className="cursor-pointer opacity-80 hover:opacity-100 hover:transition-opacity"
+            className="cursor-pointer md:w-auto md:h-auto w-40 opacity-80 hover:opacity-100 hover:transition-opacity"
           />
         </div>
         {/* Experience client and projects completed */}
         <div className="lg:w-1/2 md:w-2/3 flex flex-col py-6 justify-around  text-gray-300">
           <div className="flex md:flex-row flex-wrap md:mx-0 md:gap-0 gap-5 md:justify-between justify-start w-full mb-10">
-            <div className="flex flex-col justify-around py-8 items-center rounded-2xl bg-gray-700/10 w-40 h-40">
+            <div className="flex flex-col justify-around py-8 items-center relative rounded-2xl bg-gray-700/10 w-40 h-40">
               <FiAward className="text-3xl mb-4 text-cyan-300" />
               <h2 className="text-base font-medium">Experience</h2>
               <p className="text-xs text-gray-300/50">2+ years of working</p>
@@ -46,11 +46,10 @@ const About = () => {
           {/* about me text */}
           <div className="text-gray-400/80">
             <p className="mb-6">
-              Hello! My name is Richard and I would describe myself as a highly
-              skilled professional with a passion for creating beautiful and
-              functional user interfaces. I have a deep understanding of
-              front-end technologies such as HTML, CSS, JavaScript, React and
-              Typescript.
+              I would describe myself as a highly skilled professional with a
+              passion for creating beautiful and functional user interfaces. I
+              have a deep understanding of front-end technologies such as HTML,
+              CSS, JavaScript, React and Typescript.
             </p>
             <p className="mb-6">
               {" "}
