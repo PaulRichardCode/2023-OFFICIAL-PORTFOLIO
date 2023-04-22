719dbbddfbddced8ec5863d304e6ac74716abe75
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-04-22 11:45:54 +0100

    done baby

--- my-app/src/jobs/jobs.jsx
+++ my-app/src/jobs/jobs.jsx
@@ -11,57 +11,58 @@ const About = () => {
           <span className="md:w- md:bg-gray-300/70 md:h-px md:ml-8 md:opacity-40 "></span>
         </h1>
 
-        <div className=" md:flex md:justify-between md:items-center">
+        <div className=" md:flex ">
           {/* Experience client and projects completed */}
+          <div className="md:flex md:justify-between md:items-center my-5">
+            <div className="lg:w-5/12 md:w-2/3 md:flex md:flex-row md:items-start flex-col py-6 justify-between text-gray-300">
+              {/* about me text */}
+              <div className="text-gray-400/80 md:mx-0 md:w-full mx-auto w-11/12 ">
+                <p className="md:mb-6 mb-2 text-sm md:text-base">
+                  Since beginning my journey as a freelance developer 2 years
+                  ago, I’ve done remote work for agencies, consulted for
+                  startups, and collaborated with talented people to create web
+                  products for both business and consumer use.
+                </p>
 
-          <div className="lg:w-5/12 md:w-2/3 md:flex md:flex-row md:items-start flex-col py-6 justify-between text-gray-300">
-            {/* about me text */}
-            <div className="text-gray-400/80 md:mx-0 md:w-full mx-auto w-11/12 ">
-              <p className="md:mb-6 mb-2 text-sm md:text-base">
-                Since beginning my journey as a freelance developer 2 years ago,
-                I’ve done remote work for agencies, consulted for startups, and
-                collaborated with talented people to create web products for
-                both business and consumer use.
-              </p>
+                <p className="md:mb-6 mb-2 text-sm md:text-base">
+                  I create successful responsive websites that are fast, easy to
+                  use, and built with best practices. The main area of my
+                  expertise is front-end development, HTML, CSS, JS, building
+                  small and medium web apps, custom plugins, features,
+                  animations, and coding interactive layouts.
+                </p>
+                <p className="md:mb-6 mb-2 text-sm md:text-base">
+                  {" "}
+                  I also have full-stack developer experience with popular
+                  open-source CMS like (WordPress, Drupal, Magento, Keystone.js
+                  and others) .
+                </p>
 
-              <p className="md:mb-6 mb-2 text-sm md:text-base">
-                I create successful responsive websites that are fast, easy to
-                use, and built with best practices. The main area of my
-                expertise is front-end development, HTML, CSS, JS, building
-                small and medium web apps, custom plugins, features, animations,
-                and coding interactive layouts.
-              </p>
-              <p className="md:mb-6 mb-2 text-sm md:text-base">
-                {" "}
-                I also have full-stack developer experience with popular
-                open-source CMS like (WordPress, Drupal, Magento, Keystone.js
-                and others) .
-              </p>
-
-              <p className="md:mb-6 mb-2 text-sm md:text-base">
-                Visit my{" "}
-                <a
-                  href="https://www.linkedin.com/in/richard-paul-11321124b/"
-                  target="_blank"
-                  className="text-cyan-300 hover:underline hover:transition-all">
-                  LinkedIn
-                </a>{" "}
-                Profile for more details or just {/* ADD A CONTACT ME HREF */}
-                {/* ADD A CONTACT ME HREF */}
-                {/* ADD A CONTACT ME HREF */}
-                <a
-                  href="https://www.linkedin.com/in/richard-paul-11321124b/"
-                  target="_blank"
-                  className="text-cyan-300 hover:underline hover:transition-all">
-                  Contact
-                </a>{" "}
-                me.
-              </p>
+                <p className="md:mb-6 mb-2 text-sm md:text-base">
+                  Visit my{" "}
+                  <a
+                    href="https://www.linkedin.com/in/richard-paul-11321124b/"
+                    target="_blank"
+                    className="text-cyan-300 hover:underline hover:transition-all">
+                    LinkedIn
+                  </a>{" "}
+                  Profile for more details or just {/* ADD A CONTACT ME HREF */}
+                  {/* ADD A CONTACT ME HREF */}
+                  {/* ADD A CONTACT ME HREF */}
+                  <a
+                    href="https://www.linkedin.com/in/richard-paul-11321124b/"
+                    target="_blank"
+                    className="text-cyan-300 hover:underline hover:transition-all">
+                    Contact
+                  </a>{" "}
+                  me.
+                </p>
+              </div>
             </div>
-          </div>
-          {/* Animation */}
-          <div className="cursor-default w-1/2">
-            <TextSphare />
+            {/* Animation */}
+            <div className="cursor-default w-1/2  ">
+              <TextSphare />
+            </div>
           </div>
         </div>
       </div>
