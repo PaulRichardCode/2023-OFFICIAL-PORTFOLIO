112788e5f51c79b308bf9449021db5a27cda4a0c
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-04-30 20:58:33 +0100

    ok

--- my-app/src/project/project.jsx
+++ my-app/src/project/project.jsx
@@ -28,22 +28,20 @@ const Project = () => {
     },
   ];
   return (
-    <div>
-      <div id="project" className="md:py-20 md:w-full md:mx-0 mx-3 mt-10">
-        <div className="md:w-3/4 md:mx-auto">
-          <h1 className="text-gray-300 md:text-3xl text-2xl md:mb-0 mb-10 mr-8 w-11/12 md:w-1/2 font-semibold tracking-tight flex items-center md:justify-start justify-center">
-            <span className="text-cyan-300 md:text-2xl text-xl pr-4">02.</span>{" "}
-            Things I have Worked on{" "}
-            <span className="lg:w-16 md:bg-gray-300/70 md:h-px md:ml-8 md:opacity-40 "></span>
-          </h1>
-          <div className="flex flex-col justify-center">
-            {images.map((image) => (
-              <div key={image.id} className="w-80 h-56">
-                <img src={image.src} alt={image.caption} />
-                <div className="caption">{image.caption}</div>
-              </div>
-            ))}
-          </div>
+    <div id="project" className="md:py-20 md:w-full md:mx-0 mx-3 mt-5">
+      <div className="md:w-3/4 md:mx-auto ">
+        <h1 className="text-gray-300 md:text-3xl text-2xl md:mb-5 mb-10 mr-8 w-11/12 md:w-1/2 font-semibold tracking-tight flex items-center md:justify-start justify-center">
+          <span className="text-cyan-300 md:text-2xl text-xl pr-4">02.</span>{" "}
+          Things I have Worked on{" "}
+          <span className="lg:w-16 md:bg-gray-300/70 md:h-px md:ml-8 md:opacity-40 "></span>
+        </h1>
+        <div className="flex flex-col justify-center">
+          {images.map((image) => (
+            <div key={image.id} className="w-80 h-56">
+              <img src={image.src} alt={image.caption} />
+              <div className="caption">{image.caption}</div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
