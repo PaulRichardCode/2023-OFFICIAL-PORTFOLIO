c2b89c24b71517a9152bc5020336c6d1941eec5c
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-10 12:28:16 +0100

    w

--- my-app/src/nav/nav.jsx
+++ my-app/src/nav/nav.jsx
@@ -83,7 +83,7 @@ const Nav = () => {
                 {/* about */}
                 <a
                   href="#about"
-                  class="cursor-pointer duration-300 block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-cyan-300 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 "
+                  class="cursor-pointer duration-300 block py-2 pl-3 pr-4 text-gray-300 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-cyan-300 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 "
                   aria-current="page">
                   <span className="text-cyan-300">01. </span>
                   About
@@ -93,7 +93,7 @@ const Nav = () => {
                 {/* jobs */}
                 <a
                   href="#jobs"
-                  class="block py-2 pl-3 pr-4 duration-300 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-cyan-300 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                  class="block py-2 pl-3 pr-4 duration-300 text-gray-300 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-cyan-300 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                   <span className="text-cyan-300">02.</span> Experience
                 </a>
               </li>
@@ -101,14 +101,14 @@ const Nav = () => {
                 {/* work done */}
                 <a
                   href="#project"
-                  class="block py-2 pl-3 pr-4 text-gray-700 duration-300 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-cyan-300 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                  class="block py-2 pl-3 pr-4 text-gray-300 duration-300 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-cyan-300 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                   <span className="text-cyan-300">03.</span> Work
                 </a>
               </li>
               <li>
                 <a
                   href="#contact"
-                  class="block py-2 pl-3 pr-4 text-gray-700 duration-300 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-cyan-300 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                  class="block py-2 pl-3 pr-4 text-gray-300 duration-300 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-cyan-300 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                   <span className="text-cyan-300">04.</span> Contact
                 </a>
               </li>
